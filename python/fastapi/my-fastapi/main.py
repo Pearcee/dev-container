0d2e73c6669e986a9d8981242d2dec79880622c8
steve pearce <sjpearce@gmail.com>
2023-08-14 13:43:29 +0000

    sql

--- python/fastapi/my-fastapi/main.py
+++ python/fastapi/my-fastapi/main.py
@@ -1,30 +1,16 @@
-# pip install "uvicorn[standard]" gunicorn fastapi
-# uvicorn main:app --reload
-from typing import Union
-
+import uvicorn
 from fastapi import FastAPI
-from pydantic import BaseModel
-
 
 app = FastAPI()
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def index():
+   return {"message": "Hello World"}
 
+@app.get("/hello/{name}")
+async def hello(name):
+   return {"name": name}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/hello/{name}/{age}")
+async def hello(name:str,age:int):
+   return {"name": name, "age":age} 
